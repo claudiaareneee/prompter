107dbec2b3e70c4a8fd93a92c7e5a8d6c689c633
Claudia Nelson <claudia.nelson@bentley.com>
2024-12-15 11:37:11 -0600

    theme provider

--- src/app/components/Prompt.tsx
+++ src/app/components/Prompt.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import "./Prompt.css";
-import { ColorValue } from '@itwin/itwinui-react';
+import { Button, ColorValue } from '@itwin/itwinui-react';
 
 export interface PromptProps {
   prompt: string,
@@ -13,8 +13,8 @@ export interface PromptProps {
 };
 
 export const Prompt = ({ prompt, color, styles, onClick }: PromptProps) => {
-  const [backgroundColor, setBackgroundColor] = useState<string>(color ?? "black");
-  const [textColor, setTextColor] = useState<string>("white");
+  const [backgroundColor, setBackgroundColor] = useState<string>(color ?? "000000");
+  const [textColor, setTextColor] = useState<string>("FFFFFF");
 
   const getRandomColor = useCallback(() => {
     const letters = '0123456789ABCDEF';
@@ -25,20 +25,23 @@ export const Prompt = ({ prompt, color, styles, onClick }: PromptProps) => {
     return color;
   }, []);
 
-  useEffect(() => {
+  const setColors = useCallback(() => { 
     const newBackground = color ? color : getRandomColor();
 
     const background = ColorValue.fromString(newBackground).toTbgr();
     const grey = ColorValue.fromString("#777777").toTbgr();
-    const newText = background < grey ? "#FFFFFF" : "#000000";
+    const newText = background > grey ? "#FFFFFF" : "#000000";
 
     setBackgroundColor(newBackground);
     setTextColor(newText);
-  }, [color, getRandomColor])
+  }, [color, getRandomColor]);
+
+  useEffect(setColors, [setColors]);
 
   const handleClick = useCallback(() => {
     onClick();
-  }, [onClick]);
+    setColors();
+  }, [onClick, setColors]);
 
-  return <button onClick={handleClick} className='prompt' style={{ backgroundColor, color: textColor, ...styles }}>{prompt}</button>
+  return <Button onClick={handleClick} className='prompt' style={{ backgroundColor, color: textColor, ...styles }}>{prompt}</Button>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 // import Image from "next/image";
 
+import { ThemeProvider } from "@itwin/itwinui-react";
 import { Prompter } from "./components/Prompter";
 
 export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Prompter />
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
+    <ThemeProvider theme="dark" themeOptions={{ applyBackground: false }}>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Prompter />
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        </footer>
+      </div>
+    </ThemeProvider>
   );
 }
